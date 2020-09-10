bb0143afbc5178d24d7420caedaedd32f6427d67
Max Weinzierl <maxwellweinzierl@gmail.com>
2020-09-10 17:00:01 -0500

    Working on efficient doc expansion

--- fix_scores.py
+++ fix_scores.py
@@ -5,7 +5,7 @@ with open('runs/expert/baseline_pass_full_doc_rerank', 'r') as fi:
 		line = line.strip().split()
 		if line:
 			fixed_score = -float(line[4])
-			line[4] = fixed_score
+			line[4] = str(fixed_score)
 			fixed_rows.append('\t'.join(line))
 
 with open('runs/expert/baseline_pass_full_doc_rerank', 'w') as fo:
