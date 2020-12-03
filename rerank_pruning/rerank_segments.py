93f8376587f3065716cece89827ba1e24486a3c6
Maxwell Weinzierl <supermaxman1@gmail.com>
2020-12-03 17:00:15 -0600

    Added reranking for TPUs

--- rerank_pruning/rerank_segments.py
+++ rerank_pruning/rerank_segments.py
@@ -251,6 +251,7 @@ if __name__ == '__main__':
     with open(output_path, 'w') as f:
         for question_id, query_segments in rerank_scores.items():
             query_segments.sort()
+            print(f'{question_id}: {len(query_segments)}')
             result = rerank_prune(query_segments, t=args.threshold, n=args.top_n_gram)
             for idx, seg in enumerate(result.top_segments):
                 rank = idx + 1
