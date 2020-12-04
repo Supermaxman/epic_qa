dec5edae58cc1d34623bf492ee038ac4c38dadb1
Maxwell Weinzierl <supermaxman1@gmail.com>
2020-12-04 13:59:09 -0600

    Added reranking for TPUs

--- expand_query/format_expand.py
+++ expand_query/format_expand.py
@@ -2,6 +2,7 @@ import torch
 import argparse
 from collections import defaultdict
 import os
+from transformers import T5Tokenizer
 
 
 def load_predictions(model_path, tokenizer):
@@ -22,29 +23,27 @@ def load_predictions(model_path, tokenizer):
 			samples.append(sample_txt)
 		answer_queries[answer_id] = samples
 
-	for answer_id, answer_queries in answer_queries.items():
-		pass
-
-	return sorted_scores
+	return answer_queries
 
 
-def save_predictions(question_scores, output_path, run_name):
+def save_predictions(answer_queries, output_path):
 	with open(output_path, 'w') as f:
-		for question_id, question_scores in question_scores.items():
-			for idx, (doc_pass_id, score) in enumerate(question_scores):
-				rank = idx + 1
-				f.write(f'{question_id}\tQ0\t{doc_pass_id}\t{rank}\t{score:.8f}\t{run_name}\n')
+		for answer_id, answer_queries in answer_queries.items():
+			aq_text = '\t'.join(answer_queries)
+			f.write(f'{answer_id}\t{aq_text}\n')
 
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_argument('-m', '--model_path', required=True)
 	parser.add_argument('-o', '--output_path', required=True)
+	parser.add_argument('-t', '--tokenizer_name', default='t5-base')
 	args = parser.parse_args()
 
 	model_path = args.model_path
 	output_path = args.output_path
-	output_name = output_path.split('/')[-1].replace('.txt', '').replace('.pred', '')
+	tokenizer_name = args.tokenizer_name
+	tokenizer = T5Tokenizer.from_pretrained(tokenizer_name)
 
-	question_scores = load_predictions(model_path)
-	save_predictions(question_scores, output_path, output_name)
+	answer_queries = load_predictions(model_path, tokenizer)
+	save_predictions(answer_queries, output_path)
