8b6d58aa31cb6f07d2f291e9417bd2f13e015777
Maxwell Weinzierl <supermaxman1@gmail.com>
2020-12-04 17:14:22 -0600

    Added reranking for TPUs

--- rqe/format_eval.py
+++ rqe/format_eval.py
@@ -19,12 +19,13 @@ def write_results(rerank_scores, output_path, output_name, threshold):
 					if entail_prob > threshold:
 						nugget_count += 1
 				sample_score = nugget_count / len(unique_samples)
-				new_q_scores.append((answer_id, answer_score, sample_score))
+				full_score = sample_score
+				new_q_scores.append((answer_id, answer_score, sample_score, full_score))
 
-			new_q_scores = list(sorted(new_q_scores, key=lambda x: x[2]))
+			new_q_scores = list(sorted(new_q_scores, key=lambda x: x[-1]))
 			rank = 1
-			for answer_id, answer_score, sample_score in new_q_scores:
-				f.write(f'{question_id}\tQ0\t{answer_id}\t{rank}\t{sample_score}\t{output_name}\n')
+			for answer_id, answer_score, sample_score, full_score in new_q_scores:
+				f.write(f'{question_id}\tQ0\t{answer_id}\t{rank}\t{full_score}\t{output_name}\n')
 				rank += 1
 
 
