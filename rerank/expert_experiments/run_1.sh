622d26f1015a14df00e98756cc99398e9094bf55
Maxwell Weinzierl <supermaxman1@gmail.com>
2020-12-03 18:57:51 -0600

    Added reranking for TPUs

---
+++ rerank/expert_experiments/run_1.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+export RUN_NAME=HLTRI_RERANK_1
+export MODEL_NAME=pt-biobert-base-msmarco-expert
+export PRE_MODEL_NAME=nboost/pt-biobert-base-msmarco
+export DATASET=expert
+
+python -m rerank.rerank \
+  --query_path data/epic_qa_prelim/${DATASET}/questions.json \
+  --collection_path data/epic_qa_prelim/${DATASET}/data \
+  --label_path data/epic_qa_prelim/prelim_judgments_corrected.json \
+  --pre_model_name ${PRE_MODEL_NAME} \
+  --model_name ${MODEL_NAME}
+
+python -m rerank.format_preds \
+  --model_path models/${MODEL_NAME} \
+  --output_path models/${MODEL_NAME}/${RUN_NAME}.pred
+
+python -m rerank.format_eval \
+  --pred_path models/${MODEL_NAME}/${RUN_NAME}.pred \
+  --output_path models/${MODEL_NAME}/${RUN_NAME}.txt
+
+python rerank/epic_eval.py \
+  data/epic_qa_prelim/prelim_judgments_corrected.json \
+  models/${MODEL_NAME}/${RUN_NAME}.txt \
+  rerank/.${DATASET}_ideal_ranking_scores.tsv \
+  --task ${DATASET}
