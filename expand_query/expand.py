116e23a3ad7e906f4f82151d7230a73453454734
Maxwell Weinzierl <supermaxman1@gmail.com>
2020-12-04 13:39:36 -0600

    Added reranking for TPUs

--- expand_query/expand.py
+++ expand_query/expand.py
@@ -48,10 +48,10 @@ if __name__ == '__main__':
   top_k = args.top_k
   num_samples = args.num_samples
 
-  is_distributed = True
+  is_distributed = False
   # export TPU_IP_ADDRESS=10.155.6.34
   # export XRT_TPU_CONFIG="tpu_worker;0;$TPU_IP_ADDRESS:8470"
-  gpus = [4, 5, 6, 7]
+  gpus = [4]
   use_tpus = False
   precision = 16 if use_tpus else 32
   # precision = 32
