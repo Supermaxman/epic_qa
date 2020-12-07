c09e6c468d6eccb8bbb6e2f9b9734fa4b9bdf4b4
Maxwell Weinzierl <supermaxman1@gmail.com>
2020-12-07 00:32:36 -0600

    Added reranking for TPUs

--- experiments/expert/run_3.sh
+++ experiments/expert/run_3.sh
@@ -55,7 +55,7 @@ python -m rgqe.format_eval \
   --results_path models/${RQE_MODEL_NAME}/${RUN_NAME}.rqe_scored \
   --rgqe_path models/${RQE_MODEL_NAME}/${RUN_NAME}.rgqe \
   --output_path models/${RQE_MODEL_NAME}/${RUN_NAME}_RGQE.txt \
-  --threshold 0.7 \
+  --threshold 0.6 \
   --overlap 0.2
 
 python rerank/epic_eval.py \
