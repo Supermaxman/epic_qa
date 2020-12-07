62aa2dce088b21aa00c4950afc8835287b1807a7
Maxwell Weinzierl <supermaxman1@gmail.com>
2020-12-07 16:22:22 -0600

    Added reranking for TPUs

--- experiments/expert/run_7.sh
+++ experiments/expert/run_7.sh
@@ -5,10 +5,9 @@ export SEARCH_RUN=passage-large
 export MODEL_NAME=rerank-expert-${SEARCH_RUN}
 export PRE_MODEL_NAME=nboost/pt-biobert-base-msmarco
 export DATASET=expert
-#export INDEX_NAME=passage_index
 export INDEX_NAME=passage_index
 export COLLECTION=epic_qa_prelim
-export SEARCH_TOP_K=4000
+export SEARCH_TOP_K=1000
 
 python search/search_index.py \
   --index_path data/${COLLECTION}/${DATASET}/indices/${INDEX_NAME} \
@@ -20,9 +19,9 @@ python search/search_index.py \
 python search/search_passage_eval.py \
   --input_path data/${COLLECTION}/${DATASET}/search/${SEARCH_RUN} \
   --label_path data/${COLLECTION}/prelim_judgments_corrected.json
-#python rerank/rerank_sentence_eval.py \
-#  --input_path data/${COLLECTION}/${DATASET}/search/${SEARCH_RUN} \
-#  --label_path data/${COLLECTION}/prelim_judgments_corrected.json
+
+
+
 
 python -m rerank.rerank \
   --query_path data/${COLLECTION}/${DATASET}/questions.json \
@@ -31,23 +30,34 @@ python -m rerank.rerank \
   --label_path data/${COLLECTION}/prelim_judgments_corrected.json \
   --pre_model_name ${PRE_MODEL_NAME} \
   --model_name ${MODEL_NAME} \
-  --max_seq_len 96 \
-  --use_tpus
+  --max_seq_len 96
+
+#python -m rerank.rerank \
+#  --query_path data/${COLLECTION}/${DATASET}/questions.json \
+#  --collection_path data/${COLLECTION}/${DATASET}/data \
+#  --passage_search_run data/${COLLECTION}/${DATASET}/search/${SEARCH_RUN} \
+#  --label_path data/${COLLECTION}/prelim_judgments_corrected.json \
+#  --pre_model_name ${PRE_MODEL_NAME} \
+#  --model_name ${MODEL_NAME} \
+#  --max_seq_len 96 \
+#  --use_tpus \
+#  --batch_size 32
 
 python -m rerank.format_preds \
   --model_path models/${MODEL_NAME} \
-  --output_path models/${MODEL_NAME}/${RUN_NAME}.pred
-
+  --output_path models/${MODEL_NAME}/${RUN_NAME}.pred \
+; \
 python -m rerank.format_eval \
   --pred_path models/${MODEL_NAME}/${RUN_NAME}.pred \
-  --output_path models/${MODEL_NAME}/${RUN_NAME}.txt
-
+  --output_path models/${MODEL_NAME}/${RUN_NAME}.txt \
+  --top_k 1000 \
+; \
 python rerank/rerank_sentence_eval.py \
   --input_path models/${MODEL_NAME}/${RUN_NAME}.txt \
-  --label_path data/${COLLECTION}/prelim_judgments_corrected.json
-
+  --label_path data/${COLLECTION}/prelim_judgments_corrected.json \
+; \
 python rerank/epic_eval.py \
   data/${COLLECTION}/prelim_judgments_corrected.json \
   models/${MODEL_NAME}/${RUN_NAME}.txt \
   rerank/.${DATASET}_ideal_ranking_scores.tsv \
-  --task ${DATASET}
+  --task ${DATASET} | tail -n 3
---
+++ rerank/split_data.py
@@ -0,0 +1,69 @@
+
+import torch
+import argparse
+from collections import defaultdict
+import os
+import json
+
+
+def load_predictions(model_path):
+	pred_list = []
+	for file_name in os.listdir(model_path):
+		if file_name.endswith('.pt'):
+			preds = torch.load(os.path.join(model_path, file_name))
+			pred_list.extend(preds)
+	question_scores = defaultdict(list)
+	# # {query_id}\tQ0\t{doc_pass_id}\t{rank}\t{score:.4f}\t{run_name}
+	for prediction in pred_list:
+		doc_pass_id = prediction['id']
+		question_id = prediction['question_id']
+		# score = prediction['pos_score']
+		score = prediction['pos_score'] - prediction['neg_score']
+		question_scores[question_id].append((doc_pass_id, score))
+
+	sorted_scores = {}
+	for question_id, q_scores in question_scores.items():
+		sorted_scores[question_id] = list(sorted(q_scores, key=lambda x: x[1], reverse=True))
+
+	return sorted_scores
+
+
+def save_predictions(question_scores, output_path, run_name):
+	with open(output_path, 'w') as f:
+		for question_id, question_scores in question_scores.items():
+			for idx, (doc_pass_id, score) in enumerate(question_scores):
+				rank = idx + 1
+				f.write(f'{question_id}\tQ0\t{doc_pass_id}\t{rank}\t{score:.8f}\t{run_name}\n')
+
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-l', '--label_path', required=True)
+	parser.add_argument('-s', '--search_path', required=True)
+	parser.add_argument('-o', '--output_path', required=True)
+	parser.add_argument('-ds', '--dataset', required=True)
+	parser.add_argument('-sr', '--split_ratio', default=0.8, type=float)
+	args = parser.parse_args()
+
+	label_path = args.label_path
+	search_path = args.search_path
+	output_path = args.output_path
+	split_ratio = args.split_ratio
+	dataset_prefix = 'C' if args.dataset.lower() == 'consumer' else 'E'
+
+	labels = []
+	with open(label_path, 'r') as f:
+		all_labels = json.load(f)
+		for label in all_labels:
+			if label['question_id'].startswith(dataset_prefix):
+				labels.append(label)
+
+	num_labels = len(labels)
+	train_idx = int(split_ratio * num_labels)
+	train_labels = labels[:train_idx]
+	val_labels = labels[train_idx:]
+	print(f'#train_qs={len(train_labels)}, #val_qs={len(val_labels)}')
+	with open(os.path.join(output_path, 'train.json')) as f:
+		json.dump(train_labels, f, indent=2)
+	with open(os.path.join(output_path, 'val.json')) as f:
+		json.dump(val_labels, f, indent=2)
