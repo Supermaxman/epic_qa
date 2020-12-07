3a7fcdd95111e7fa1dead4898334a9c525e5fdb4
Maxwell Weinzierl <supermaxman1@gmail.com>
2020-12-06 22:44:11 -0600

    Added reranking for TPUs

--- rqe/format_rqe.py
+++ rqe/format_rqe.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 import json
 
 
-def load_predictions(model_path, answer_samples, threshold):
+def load_predictions(model_path, answer_samples, threshold, num_samples):
 	pred_list = []
 	for file_name in os.listdir(model_path):
 		if file_name.endswith('.pt'):
@@ -14,6 +14,7 @@ def load_predictions(model_path, answer_samples, threshold):
 			pred_list.extend(preds)
 	query_answer_sample_probs = defaultdict(lambda: defaultdict(list))
 	probs = []
+	num_examples = 0
 	for prediction in tqdm(pred_list):
 		answer_id = prediction['id']
 		question_id = prediction['question_id']
@@ -27,10 +28,13 @@ def load_predictions(model_path, answer_samples, threshold):
 			'entail_prob': entail_prob,
 			'sample_text': sample_text
 		}
-		query_answer_sample_probs[question_id][answer_id].append(sample_data)
+		if len(query_answer_sample_probs[question_id][answer_id]) < num_samples:
+			query_answer_sample_probs[question_id][answer_id].append(sample_data)
+			num_examples += 1
 		probs.append(entail_prob)
 	print(f'min={min(probs)}')
 	print(f'max={max(probs)}')
+	print(f'num_examples={num_examples}')
 	return query_answer_sample_probs
 
 
@@ -45,15 +49,17 @@ if __name__ == '__main__':
 	parser.add_argument('-e', '--expand_path', required=True)
 	parser.add_argument('-o', '--output_path', required=True)
 	parser.add_argument('-t', '--threshold', default=0.5, type=float)
+	parser.add_argument('-k', '--num_samples', default=3, type=int)
 	args = parser.parse_args()
 
 	model_path = args.model_path
 	expand_path = args.expand_path
 	output_path = args.output_path
 	threshold = args.threshold
+	num_samples = args.num_samples
 
 	with open(expand_path, 'r') as f:
 		answer_samples = json.load(f)
 
-	query_answer_sample_probs = load_predictions(model_path, answer_samples, threshold)
+	query_answer_sample_probs = load_predictions(model_path, answer_samples, threshold, num_samples)
 	save_predictions(query_answer_sample_probs, output_path)
