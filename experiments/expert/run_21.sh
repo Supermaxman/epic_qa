b62ef07bba706f2fbe709e0a9e17548b1110b3ea
Maxwell Weinzierl <supermaxman1@gmail.com>
2020-12-08 21:54:17 -0600

    Added reranking for TPUs

--- experiments/expert/run_21.sh
+++ experiments/expert/run_21.sh
@@ -9,21 +9,21 @@ export RQE_MODEL_NAME=quora-seq-nboost-pt-bert-base-uncased-msmarco
 export EXP_PRE_MODEL_NAME=models/docT5query-base/model.ckpt-1004000
 export DATASET=expert
 export COLLECTION=epic_qa_prelim
-export RQE_THRESHOLD=0.8
+export RQE_THRESHOLD=0.7
 
 # create expanded questions for every answer
-python -m expand_query.expand \
- --input_path models/${RERANK_MODEL_NAME}/${RERANK_RUN_NAME}.txt \
- --collection_path data/${COLLECTION}/${DATASET}/data \
- --pre_model_name ${EXP_PRE_MODEL_NAME} \
- --model_name ${EXP_MODEL_NAME} \
- --top_k 20 \
- --num_samples 20 \
- --batch_size 16 \
-; \
-python -m expand_query.format_expand \
-  --model_path models/${EXP_MODEL_NAME} \
-  --output_path models/${EXP_MODEL_NAME}/${RUN_NAME}.exp \
+#python -m expand_query.expand \
+# --input_path models/${RERANK_MODEL_NAME}/${RERANK_RUN_NAME}.txt \
+# --collection_path data/${COLLECTION}/${DATASET}/data \
+# --pre_model_name ${EXP_PRE_MODEL_NAME} \
+# --model_name ${EXP_MODEL_NAME} \
+# --top_k 20 \
+# --num_samples 20 \
+# --batch_size 16 \
+#; \
+#python -m expand_query.format_expand \
+#  --model_path models/${EXP_MODEL_NAME} \
+#  --output_path models/${EXP_MODEL_NAME}/${RUN_NAME}.exp
 
 # self entailment
 python -m rgqe.rgqe \
@@ -75,33 +75,34 @@ python -m rgqe.rgqe_top_components \
   --output_path models/${RQE_MODEL_NAME}/${RUN_NAME}.rgqe_top_cc \
   --threshold ${RQE_THRESHOLD}
 
-python -m rgqe.rgqe \
-  --input_path models/${RQE_MODEL_NAME}/${RUN_NAME}.rqe_scored \
-  --model_name models/${RQE_MODEL_NAME} \
-  --mode all \
-; \
-python -m rgqe.format_rgqe \
-  --model_path models/${RQE_MODEL_NAME} \
-  --output_path models/${RQE_MODEL_NAME}/${RUN_NAME}.rgqe \
-  --threshold 0.5
-
-python -m rgqe.format_eval \
-  --results_path models/${RQE_MODEL_NAME}/${RUN_NAME}.rqe_scored \
-  --rgqe_path models/${RQE_MODEL_NAME}/${RUN_NAME}.rgqe \
-  --output_path models/${RQE_MODEL_NAME}/${RUN_NAME}_RGQE.txt \
-  --threshold 0.5 \
-  --overlap_ratio 1.0 \
-  --overall_ratio 0.0 \
-; \
-python rerank/epic_eval.py \
-  data/${COLLECTION}/${DATASET}/split/val.json \
-  models/${RQE_MODEL_NAME}/${RUN_NAME}.txt \
-  rerank/.${DATASET}_ideal_ranking_scores.tsv \
-  --task ${DATASET} \
-  | tail -n 3 \
-  | awk \
-    '{ for (i=1; i<=NF; i++) RtoC[i]= (RtoC[i]? RtoC[i] FS $i: $i) }
-    END{ for (i in RtoC) print RtoC[i] }' \
-  | tail -n 2 > models/${RQE_MODEL_NAME}/${RUN_NAME}.eval \
-; \
-cat models/${RQE_MODEL_NAME}/${RUN_NAME}.eval
+# all entailment against sets
+#python -m rgqe.rgqe \
+#  --input_path models/${RQE_MODEL_NAME}/${RUN_NAME}.rqe_scored \
+#  --model_name models/${RQE_MODEL_NAME} \
+#  --mode all \
+#; \
+#python -m rgqe.format_rgqe \
+#  --model_path models/${RQE_MODEL_NAME} \
+#  --output_path models/${RQE_MODEL_NAME}/${RUN_NAME}.rgqe \
+#  --threshold 0.5
+#
+#python -m rgqe.format_eval \
+#  --results_path models/${RQE_MODEL_NAME}/${RUN_NAME}.rqe_scored \
+#  --rgqe_path models/${RQE_MODEL_NAME}/${RUN_NAME}.rgqe \
+#  --output_path models/${RQE_MODEL_NAME}/${RUN_NAME}_RGQE.txt \
+#  --threshold 0.5 \
+#  --overlap_ratio 1.0 \
+#  --overall_ratio 0.0 \
+#; \
+#python rerank/epic_eval.py \
+#  data/${COLLECTION}/${DATASET}/split/val.json \
+#  models/${RQE_MODEL_NAME}/${RUN_NAME}.txt \
+#  rerank/.${DATASET}_ideal_ranking_scores.tsv \
+#  --task ${DATASET} \
+#  | tail -n 3 \
+#  | awk \
+#    '{ for (i=1; i<=NF; i++) RtoC[i]= (RtoC[i]? RtoC[i] FS $i: $i) }
+#    END{ for (i in RtoC) print RtoC[i] }' \
+#  | tail -n 2 > models/${RQE_MODEL_NAME}/${RUN_NAME}.eval \
+#; \
+#cat models/${RQE_MODEL_NAME}/${RUN_NAME}.eval
