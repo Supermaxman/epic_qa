827016ce374352e607761ebfc7f7774ef4f7a4e5
Maxwell Weinzierl <supermaxman1@gmail.com>
2020-12-09 01:14:41 -0600

    Added reranking for TPUs

--- rgqe/format_eval.py
+++ rgqe/format_eval.py
@@ -7,7 +7,7 @@ def write_run(results, output_path, output_name):
 	with open(output_path, 'w') as f:
 		for question_id, q_results in results.items():
 			rank = 1
-			for answer in q_results:
+			for answer in q_results['answers']:
 				answer_id = answer['answer_id']
 				answer_score = answer['score']
 				f.write(f'{question_id}\tQ0\t{answer_id}\t{rank}\t{answer_score:.8f}\t{output_name}\n')
@@ -28,8 +28,7 @@ if __name__ == '__main__':
 	output_name = output_path.split('/')[-1].replace('.txt', '').replace('.pred', '')
 
 	with open(results_path) as f:
-		results = json.load(f)
-		rerank_results = results['answers']
+		rerank_results = json.load(f)
 
 	write_run(rerank_results, output_path, output_name)
 
