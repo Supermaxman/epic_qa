00c1628d62ba9cfa0fcfcf7a9b3d690e46a5c0db
Maxwell Weinzierl <supermaxman1@gmail.com>
2020-12-09 17:41:25 -0600

    Added reranking for TPUs

--- experiments/expert/regeques_1.sh
+++ experiments/expert/regeques_1.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# run names
 export EXP_DATA_RUN_NAME=HLTRI_REGEQUES_EXP_DATA_1
 export INDEX_NAME=HLTRI_REGEQUES_EXP_INDEX_1
 export SEARCH_RUN_NAME=HLTRI_REGEQUES_SEARCH_1
@@ -7,30 +8,52 @@ export RERANK_RUN_NAME=HLTRI_REGEQUES_RERANK_1
 export EXP_ANSWER_RUN_NAME=HLTRI_REGEQUES_EXP_ANSWER_1
 export RQE_RUN_NAME=HLTRI_REGEQUES_1
 
+# collection and task names
 export COLLECTION=epic_qa_prelim
 export DATASET=expert
 
+# major hyper-parameters for system
 export SEARCH_TOP_K=1000
-export NEGATIVE_SAMPLES=800
+#export NEGATIVE_SAMPLES=800
+export NEGATIVE_SAMPLES=400
 export RGQE_TOP_K=100
 export RGQE_THRESHOLD=0.8
 export RQE_THRESHOLD=0.01
 export RGQE_RATIO=0.8
 export MAX_RQE_SEQ_LEN=64
+export EXP_ANSWER_TOP_K=20
+export EXP_ANSWER_NUM_SAMPLES=20
+export EXP_ANSWER_BATCH_SIZE=16
 
+# flags to avoid re-running certain components
+# index & search flags
 export CREATE_INDEX=false
 export EXPAND_INDEX=false
 export SEARCH_INDEX=false
-export TRAIN_RERANK=false
+
+# rerank flags
+# RERANK fine-tune reranking model using training set
+export TRAIN_RERANK=true
+# RERANK run rerank using trained model on validation set
 export RUN_RERANK=true
+# RERANK run evaluation script on validation set
 export EVAL_RERANK=true
+
+# rerank answer query expansion flags
 export RUN_EXPAND_ANSWERS=true
-export RUN_RGQE_SELF=true
-export RUN_RGQE_QUESTION=true
-export RUN_RGQE_TOP=true
-export RUN_RGQE_ALL=true
-export RUN_RGQE_ALL_CC=true
-export EVAL_RGQE=true
+
+# RGQE pairwise self-entailment to find entailed sets for each answer
+export RUN_RGQE_SELF=false
+# RGQE query-generated question entailment to filter poor generated questions
+export RUN_RGQE_QUESTION=false
+# RGQE full set-pairwise entailment for top_k answers for each query
+export RUN_RGQE_TOP=false
+# RGQE top_k set entailment to all set entailment to find entailed sets for all answers
+export RUN_RGQE_ALL=false
+# RGQE rerank answers based on generated question entailment sets
+export RUN_RGQE_RERANK=false
+# RGQE run evaluation script on validation set
+export EVAL_RGQE=false
 
 export RERANK_MODEL_NAME=rerank-expert-${RERANK_RUN_NAME}
 #export RERANK_MODEL_NAME=rerank-expert-passage-large-HLTRI_RERANK_15
@@ -76,7 +99,7 @@ export RGQE_TOP_CC_FILE_PATH=${RGQE_TOP_PATH}/${RQE_RUN_NAME}.rgqe_top_cc
 
 export RGQE_ALL_PATH=${ARTIFACTS_PATH}/${RQE_RUN_NAME}_ALL
 export RGQE_ALL_FILE_PATH=${RGQE_ALL_PATH}/${RQE_RUN_NAME}.rgqe_all
-export RGQE_ALL_CC_SCORED_FILE_PATH=${RGQE_ALL_PATH}/${RQE_RUN_NAME}.rgqe_all_cc_scored
+export RGQE_ALL_RERANK_FILE_PATH=${RGQE_ALL_PATH}/${RQE_RUN_NAME}.rgqe_rerank
 
 export RERANK_RUN_PATH=${RERANK_PATH}/${RERANK_RUN_NAME}.txt
 export ANSWERS_PATH=${RERANK_PATH}/${RERANK_RUN_NAME}.answers
@@ -122,7 +145,7 @@ if [[ ${CREATE_INDEX} = true ]]; then
 
         mkdir ${EXP_DATA_PATH}/data_expanded
         python search/expand_passages_jsonl.py \
-          --collection_path ${DATASET_PATH}/data.jsonl \
+          --collection_path ${COLLECTION_JSONL_FILE_PATH} \
           --expand_path ${EXP_DATA_FILE_PATH} \
           --output_path ${EXP_DATA_PATH}/data_expanded/data_expanded.jsonl
 
@@ -139,7 +162,7 @@ if [[ ${CREATE_INDEX} = true ]]; then
     else
         mkdir ${COLLECTION_JSONL_PATH}/data
         python search/expand_passages_jsonl.py \
-          --collection_path ${DATASET_PATH}/data.jsonl \
+          --collection_path ${COLLECTION_JSONL_FILE_PATH} \
           --output_path ${COLLECTION_JSONL_PATH}/data/data.jsonl
         # index dataset
         python -m pyserini.index \
@@ -220,13 +243,13 @@ if [[ ${EVAL_RERANK} = true ]]; then
       ${RERANK_RUN_PATH} \
       rerank/.${DATASET}_ideal_ranking_scores.tsv \
       --task ${DATASET} \
-      | tail -n 3 \
+      > ${RERANK_EVAL_PATH} \
+      ; \
+      tail -n 3 ${RERANK_EVAL_PATH} \
       | awk \
         '{ for (i=1; i<=NF; i++) RtoC[i]= (RtoC[i]? RtoC[i] FS $i: $i) }
         END{ for (i in RtoC) print RtoC[i] }' \
-      | tail -n 2 > ${RERANK_EVAL_PATH} \
-    ; \
-    cat ${RERANK_EVAL_PATH}
+      | tail -n 2
 fi
 
 
@@ -238,9 +261,9 @@ if [[ ${RUN_EXPAND_ANSWERS} = true ]]; then
      --collection_path ${COLLECTION_PATH} \
      --pre_model_name ${EXP_PRE_MODEL_NAME} \
      --model_name ${EXP_MODEL_NAME} \
-     --top_k 20 \
-     --num_samples 20 \
-     --batch_size 16 \
+     --top_k ${EXP_ANSWER_TOP_K} \
+     --num_samples ${EXP_ANSWER_NUM_SAMPLES} \
+     --batch_size ${EXP_ANSWER_BATCH_SIZE} \
     ; \
     python -m expand_query.format_expand \
       --input_path ${EXP_ANSWER_PATH} \
@@ -328,19 +351,19 @@ if [[ ${RUN_RGQE_ALL} = true ]]; then
       --output_path ${RGQE_ALL_FILE_PATH}
 fi
 
-if [[ ${RUN_RGQE_ALL_CC} = true ]]; then
-    echo "Running RGQE ALL CC..."
-    python -m rgqe.rgqe_all_components \
+if [[ ${RUN_RGQE_RERANK} = true ]]; then
+    echo "Running RGQE rerank..."
+    python -m rgqe.rgqe_rerank \
       --input_path ${RGQE_ALL_FILE_PATH} \
       --cc_path ${RGQE_TOP_CC_FILE_PATH} \
       --answers_path ${ANSWERS_PATH} \
       --queries_path ${QUERY_PATH} \
-      --output_path ${RGQE_ALL_CC_SCORED_FILE_PATH} \
+      --output_path ${RGQE_ALL_RERANK_FILE_PATH} \
       --threshold ${RGQE_THRESHOLD} \
       --ratio ${RGQE_RATIO} \
     ; \
     python -m rgqe.format_eval \
-      --results_path ${RGQE_ALL_CC_SCORED_FILE_PATH} \
+      --results_path ${RGQE_ALL_RERANK_FILE_PATH} \
       --output_path ${RUN_PATH}
 fi
 
@@ -351,12 +374,12 @@ if [[ ${EVAL_RGQE} = true ]]; then
       ${RUN_PATH} \
       rerank/.${DATASET}_ideal_ranking_scores.tsv \
       --task ${DATASET} \
-      | tail -n 3 \
+      > ${EVAL_PATH} \
+      ; \
+      tail -n 3 ${EVAL_PATH} \
       | awk \
         '{ for (i=1; i<=NF; i++) RtoC[i]= (RtoC[i]? RtoC[i] FS $i: $i) }
         END{ for (i in RtoC) print RtoC[i] }' \
-      | tail -n 2 > ${EVAL_PATH} \
-    ; \
-    cat ${EVAL_PATH}
+      | tail -n 2
 fi
 
