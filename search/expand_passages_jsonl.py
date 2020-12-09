303de13f5ec1d12b842db48b60f605cc2bb5af8f
Maxwell Weinzierl <supermaxman1@gmail.com>
2020-12-09 17:13:37 -0600

    Added reranking for TPUs

--- search/expand_passages_jsonl.py
+++ search/expand_passages_jsonl.py
@@ -9,7 +9,7 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('-c', '--collection_path', required=True)
 parser.add_argument('-o', '--output_path', required=True)
-parser.add_argument('-ex', '--expand_path', required=True)
+parser.add_argument('-ex', '--expand_path', default=None)
 
 args = parser.parse_args()
 
@@ -20,13 +20,14 @@ output_path = args.output_path
 expand_path = args.expand_path
 
 expand_lookup = {}
-print(f'Loading expanded questions...')
-with open(expand_path, 'r') as f:
-  for line in f:
-    line = line.strip()
-    if line:
-      passage = json.loads(line)
-      expand_lookup[passage['answer_id']] = passage['samples']
+if expand_path is not None:
+  print(f'Loading expanded questions...')
+  with open(expand_path, 'r') as f:
+    for line in f:
+      line = line.strip()
+      if line:
+        passage = json.loads(line)
+        expand_lookup[passage['answer_id']] = passage['samples']
 
 print(f'Creating collection...')
 with open(collection_path, 'r') as fi:
@@ -38,9 +39,10 @@ with open(collection_path, 'r') as fi:
         passage_id = passage['context_id']
         passage_text = passage['text']
         answer_id = passage['sentences'][0]['sentence_id'] + ':' + passage['sentences'][-1]['sentence_id']
-        expand_samples = expand_lookup[answer_id]
-        expand_text = ' '.join(expand_samples)
-        passage_text += ' ' + expand_text
+        if expand_path is not None:
+          expand_samples = expand_lookup[answer_id]
+          expand_text = ' '.join(expand_samples)
+          passage_text += ' ' + expand_text
         fo.write(
           json.dumps({
             'id': passage_id,
