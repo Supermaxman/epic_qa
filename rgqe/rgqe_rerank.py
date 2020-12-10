0c705613e164aa18a7006f33755e270ee6a538dd
Maxwell Weinzierl <supermaxman1@gmail.com>
2020-12-10 16:27:17 -0600

    Modified to make question answer sets independent

--- rgqe/rgqe_rerank.py
+++ rgqe/rgqe_rerank.py
@@ -97,14 +97,14 @@ if __name__ == '__main__':
 				if rerank_score > 0:
 					new_score = (ratio**(num_entailed + 1)) * rerank_score
 				else:
-					new_score = ((1.0-ratio)**(num_entailed + 1)) * rerank_score
+					new_score = ((1.0+(1.0 - ratio))**(num_entailed + 1)) * rerank_score
 				num_modified += 1
 			else:
-				if rerank_score > 0:
-					new_score = ((1.0-ratio)**(num_entailed)) * rerank_score
-				else:
-					new_score = (ratio**(num_entailed)) * rerank_score
-				# new_score = rerank_score
+				# if rerank_score > 0:
+				# 	new_score = ((1.0-ratio)**(num_entailed)) * rerank_score
+				# else:
+				# 	new_score = (ratio**(num_entailed)) * rerank_score
+				new_score = rerank_score
 			if answer['rank'] <= 100:
 				top_100_set_counts.append(num_entailed)
 			else:
