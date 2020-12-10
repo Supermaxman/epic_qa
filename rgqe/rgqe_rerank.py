9631b84624f0a7fa1754c06b8395836fd408a0d2
Maxwell Weinzierl <supermaxman1@gmail.com>
2020-12-10 16:32:22 -0600

    Modified to make question answer sets independent

--- rgqe/rgqe_rerank.py
+++ rgqe/rgqe_rerank.py
@@ -94,11 +94,12 @@ if __name__ == '__main__':
 			novel_sets = entailed_sets.difference(overlap_set)
 			novel_count = len(novel_sets)
 			if novel_count == 0:
-				# if positive score reduce by ratio ^ (num_entailed + 1)
+				# if positive score make less positive by %ratio ^ (num_entailed + 1)
 				if rerank_score > 0:
-					new_score = (ratio**(1)) * rerank_score
+					new_score = (ratio**(num_entailed + 1)) * rerank_score
+				# if negative score make more negative by %(1.0 + (1.0 - ratio) ^ (num_entailed + 1)
 				else:
-					new_score = ((1.0+(1.0 - ratio))**(1)) * rerank_score
+					new_score = ((1.0+(1.0 - ratio))**(num_entailed + 1)) * rerank_score
 				num_modified += 1
 			else:
 				new_score = rerank_score
