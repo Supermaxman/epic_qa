e8a73bb5eb45200cd55c95509f03617825482eba
Maxwell Weinzierl <supermaxman1@gmail.com>
2020-12-11 08:33:57 -0600

    RGQE rerank experiments

--- rgqe/rgqe_rerank.py
+++ rgqe/rgqe_rerank.py
@@ -120,10 +120,10 @@ if __name__ == '__main__':
 				set_score = rerank_score / num_entailed
 				for entailed_set_id in entailed_sets:
 					novelty_score = qa_entailed_set_scores[entailed_set_id]
-					if rerank_score > 0:
-						new_score += 0.5 * set_score + 0.5 * novelty_score * set_score
-					else:
-						new_score += 0.5 * set_score + (1.0 - (1.0 - novelty_score)) * set_score
+					new_score += 0.5 * set_score + 0.5 * novelty_score * set_score
+					# if rerank_score > 0:
+					# else:
+					# 	new_score += 0.5 * set_score + (1.0 - (1.0 - novelty_score)) * set_score
 			else:
 				new_score = rerank_score
 
