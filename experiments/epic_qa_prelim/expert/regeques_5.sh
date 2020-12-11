ab425b8a6fa2b99806cc2010afe54f4268906b4c
Maxwell Weinzierl <supermaxman1@gmail.com>
2020-12-11 00:26:57 -0600

    Modified to make question answer sets independent

--- experiments/epic_qa_prelim/expert/regeques_5.sh
+++ experiments/epic_qa_prelim/expert/regeques_5.sh
@@ -17,7 +17,7 @@ export DATASET=expert
 # major hyper-parameters for system
 export SEARCH_TOP_K=500
 export NEGATIVE_SAMPLES=800
-export RGQE_TOP_K=50
+export RGQE_TOP_K=100
 export RGQE_SELF_THRESHOLD=0.8
 export RGQE_TOP_C_THRESHOLD=0.8
 export RGQE_ALL_THRESHOLD=0.8
