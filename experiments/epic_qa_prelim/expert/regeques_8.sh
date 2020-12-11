e186c5f30bb6f4d7cc71dd04db9610c089fd087c
Maxwell Weinzierl <supermaxman1@gmail.com>
2020-12-11 17:59:49 -0600

    Changed top components to require at least two answers to keep

--- experiments/epic_qa_prelim/expert/regeques_8.sh
+++ experiments/epic_qa_prelim/expert/regeques_8.sh
@@ -51,11 +51,11 @@ export EVAL_RERANK=false
 export RUN_EXPAND_ANSWERS=false
 
 # RGQE pairwise self-entailment to find entailed sets for each answer
-export RUN_RGQE_SELF=false
+export RUN_RGQE_SELF=true
 # RGQE query-generated question entailment to filter poor generated questions
-export RUN_RGQE_QUESTION=false
+export RUN_RGQE_QUESTION=true
 # RGQE full set-pairwise entailment for top_k answers for each query
-export RUN_RGQE_TOP=false
+export RUN_RGQE_TOP=true
 # RGQE rerank answers based on generated question entailment sets
 export RUN_RGQE_RERANK=true
 # RGQE run evaluation script on validation set
