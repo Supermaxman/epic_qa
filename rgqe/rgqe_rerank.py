82f38a48116d1ec8b797efc9628522b7339e114f
Maxwell Weinzierl <supermaxman1@gmail.com>
2020-12-11 17:40:51 -0600

    Changed top components to require at least two answers to keep

--- rgqe/rgqe_rerank.py
+++ rgqe/rgqe_rerank.py
@@ -87,21 +87,29 @@ if __name__ == '__main__':
 			answer['entailed_sets'] = qa_sets
 			answer['text'] = answer_text_lookup[answer_id]
 			answer['entailed_sets_text'] = [entailed_sets_text[x] for x in qa_sets]
-		print(f'{question_id}: {num_answers_with_set / len(question_answers):.2f}% '
-					f'percent answers with at least one entailed set')
 
 		ranking = get_ranking(question_id, top_answers, entailed_sets_text)
+		num_top_zero = 0
 		ndns_rank = 0
 		for idx, ndns_scored_answer in enumerate(ranking.answers):
 			a_answer = ndns_scored_answer.answer
 			answer_id = f'{a_answer.start_sent_id}:{a_answer.end_sent_id}'
 			answer = answer_lookup[answer_id]
-			answer['ndns_gain'] = ndns_scored_answer.gain
+			ndns_gain = ndns_scored_answer.gain
+			answer['ndns_gain'] = ndns_gain
+			# TODO two ideas:
+			# TODO 1. use bert score if gain is 0, not arbitrary ranking
+			#
+			if ndns_gain == 0:
+				max_non_top_score = max(max_non_top_score, answer['score'])
+				num_top_zero += 1
 			# goes from 1.0 to 0.0
 			# add max non top answer score to make sure ndns is ranked higher
 			answer['ndns_score'] = (1.0 - (ndns_rank / len(ranking.answers))) + max_non_top_score
 			ndns_rank += 1
 
+		print(f'{question_id}: {num_answers_with_set / len(question_answers):.2f}% '
+					f'percent answers with at least one entailed set (num_top_zero={num_top_zero})')
 		for answer in question_answers:
 			if 'ndns_score' not in answer:
 				answer['ndns_score'] = answer['score']
