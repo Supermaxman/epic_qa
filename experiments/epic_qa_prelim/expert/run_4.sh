70992c47b134bcb04079a514e50bbc5785e1be02
Max Weinzierl <maxwellweinzierl@gmail.com>
2021-05-08 22:15:35 -0500

    New work for CIKM

--- experiments/epic_qa_prelim/expert/run_4.sh
+++ experiments/epic_qa_prelim/expert/run_4.sh
@@ -20,9 +20,9 @@ export SEARCH_TOP_K=500
 export NEGATIVE_SAMPLES=800
 export RGQE_TOP_K=100
 # 0.6 for run_3
-export RGQE_SELF_THRESHOLD=0.6
+export RGQE_SELF_THRESHOLD=0.9
 # 0.6 for run_3
-export RGQE_TOP_C_THRESHOLD=0.6
+export RGQE_TOP_C_THRESHOLD=0.9
 # 0.01 for run_3
 export RQE_TOP_THRESHOLD=0.01
 export RGQE_RATIO=0.9
@@ -285,15 +285,15 @@ fi
 if [[ ${RUN_RGQE_SELF} = true ]]; then
     echo "Running self RGQE..."
     # self entailment
-    python rgqe/rgqe.py \
-      --input_path ${EXP_ANSWER_FILE_PATH} \
-      --output_path ${RGQE_SELF_PATH} \
-      --model_name ${RQE_MODEL_NAME} \
-      --pre_model_name ${RQE_PRE_MODEL_NAME} \
-      --max_seq_len ${RGQE_SEQ_LEN} \
-      --batch_size ${RGQE_BATCH_SIZE} \
-      --mode self \
-    ; \
+#    python rgqe/rgqe.py \
+#      --input_path ${EXP_ANSWER_FILE_PATH} \
+#      --output_path ${RGQE_SELF_PATH} \
+#      --model_name ${RQE_MODEL_NAME} \
+#      --pre_model_name ${RQE_PRE_MODEL_NAME} \
+#      --max_seq_len ${RGQE_SEQ_LEN} \
+#      --batch_size ${RGQE_BATCH_SIZE} \
+#      --mode self \
+#    ; \
     python rgqe/format_rgqe_self.py \
       --input_path ${RGQE_SELF_PATH} \
       --output_path ${RGQE_SELF_FILE_PATH} \
