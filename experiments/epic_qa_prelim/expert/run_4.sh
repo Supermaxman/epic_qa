dcc8d48a69900601b78f2e00177e896dacc7aad9
Max Weinzierl <maxwellweinzierl@gmail.com>
2021-05-11 12:10:05 -0500

    New work for CIKM

--- experiments/epic_qa_prelim/expert/run_4.sh
+++ experiments/epic_qa_prelim/expert/run_4.sh
@@ -21,11 +21,11 @@ export NEGATIVE_SAMPLES=800
 # 100 for run_3, 20
 export RGQE_TOP_K=20
 # 0.6 for run_3, 0.6
-export RGQE_SELF_THRESHOLD=0.7
+export RGQE_SELF_THRESHOLD=0.6
 # 0.6 for run_3, 0.6
-export RGQE_TOP_C_THRESHOLD=0.7
+export RGQE_TOP_C_THRESHOLD=0.6
 # 0.01 for run_3, 0.0
-export RQE_TOP_THRESHOLD=0.20
+export RQE_TOP_THRESHOLD=0.05
 export RGQE_RATIO=0.9
 export RGQE_SEQ_LEN=96
 export RGQE_BATCH_SIZE=64
