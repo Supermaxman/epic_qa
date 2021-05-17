d0c635fa516f2ada64752e9d13339c26576303cf
Maxwell Weinzierl <supermaxman1@gmail.com>
2021-05-17 17:38:04 -0500

    New work for CIKM

--- rgqe/rgqe_top_question_components.py
+++ rgqe/rgqe_top_question_components.py
@@ -86,9 +86,9 @@ def create_components(question_entail_set_pairs, question_answer_sets, cc_thresh
 					# TODO added for CIKM for sparse questions
 					# new_entailed_set_id = merged_mapping[entailed_set_id]
 					# new_entailed_sets.add(new_entailed_set_id)
-				# else:
-				new_entailed_set_id = merged_mapping[entailed_set_id]
-				new_entailed_sets.add(new_entailed_set_id)
+				else:
+					new_entailed_set_id = merged_mapping[entailed_set_id]
+					new_entailed_sets.add(new_entailed_set_id)
 			if num_unconnected != len(a_sets):
 				connected_answers += 1
 			merged_entailed_set_answer_lookup[answer_id] = sorted(list(new_entailed_sets))
